93f17b81008a3b42d874a098b53d94e5e8f7aedd
21-ko (happy_land) <77537111+21-ko@users.noreply.github.com>
2024-09-14 00:59:23 +0900

    Delete TEST.C

--- TEST.C
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("This program is an application for FORIS-OS.\n");
-    printf("It cannot be started on this OS.\n");
-    printf("Please run it on FORIS-OS.\n");
-    return 0;
-}
